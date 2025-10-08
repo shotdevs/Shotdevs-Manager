2a050eebc406936be029e7c5c98a53a2dd120970
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2025-10-08 20:26:59 +0530

    Update deploycommands.js

--- deploycommands.js
+++ deploycommands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config();
+require('dotenv').config(); // This loads the .env file
 
 const commands = [];
 const foldersPath = path.join(__dirname, 'commands');
@@ -19,17 +19,24 @@ for (const folder of commandFolders) {
     }
 }
 
+// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
+// and deploy your commands!
 (async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
+            // This line now correctly uses the IDs from your .env file
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
         );
+
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
+        // And of course, make sure you catch and log any errors!
         console.error(error);
     }
 })();
