eae0d17981062084442e4c53088a7f8d118cea7e
Shibindevs <shibinhussainmk@gmail.com>
2025-10-10 08:19:26 +0530

    Delete events/MemberProfile.js

--- events/MemberProfile.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const memberProfileSchema = new Schema({
-    guildId: { type: String, required: true },
-    userId: { type: String, required: true },
-    warnings: { type: Array, default: [] },
-});
-
-module.exports = model('MemberProfile', memberProfileSchema);
