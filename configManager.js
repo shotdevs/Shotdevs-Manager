a55048c08c55c6ae4a8e5c5ebcae85a2a345e1e0
Shibindevs <shibinhussainmk@gmail.com>
2025-10-10 06:41:53 +0530

    Update configManager.js

--- configManager.js
+++ configManager.js
@@ -1,41 +1,23 @@
-const fs = require('node:fs').promises; // <-- Use the promises version of fs
-const path = require('node:path');
-
-const configPath = path.join(__dirname, 'guild_configs.json');
-
-// This is now an async function
-async function readConfigs() {
-    try {
-        const data = await fs.readFile(configPath, 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        // If file doesn't exist or is empty, return empty object
-        if (error.code === 'ENOENT') {
-            return {};
-        }
-        throw error;
-    }
-}
-
-// This is now an async function
-async function writeConfigs(data) {
-    await fs.writeFile(configPath, JSON.stringify(data, null, 4));
-}
+const GuildConfig = require('./models/GuildConfig.js');
 
 module.exports = {
-    // This function is now async
     getConfig: async function(guildId) {
-        const configs = await readConfigs();
-        return configs[guildId] || {};
+        // Find the config for this guild, or create a new one if it doesn't exist
+        const config = await GuildConfig.findOneAndUpdate(
+            { guildId: guildId },
+            {}, // No updates needed, just find or create
+            { upsert: true, new: true } // upsert:true creates if not found
+        );
+        return config;
     },
 
-    // This function is now async
     setConfig: async function(guildId, key, value) {
-        const configs = await readConfigs();
-        if (!configs[guildId]) {
-            configs[guildId] = {};
-        }
-        configs[guildId][key] = value;
-        await writeConfigs(configs);
+        // Find the config and update a specific key-value pair
+        const config = await GuildConfig.findOneAndUpdate(
+            { guildId: guildId },
+            { [key]: value },
+            { upsert: true, new: true }
+        );
+        return config;
     }
 };
