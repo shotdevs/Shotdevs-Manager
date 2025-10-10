8e3c8239b3227e7f8adb3fc53844cecb36246087
Shibindevs <shibinhussainmk@gmail.com>
2025-10-10 07:32:52 +0530

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,8 +4,12 @@ const { getConfig } = require('../configManager');
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
-        const config = getConfig(member.guild.id);
+        // FIXED: Added 'await' to get the configuration from the database
+        const config = await getConfig(member.guild.id);
+
+        // This check is fine, it will now work correctly
         if (!config.welcomeEnabled) return;
+
         // Send welcome message
         if (config.welcomeChannelId && config.welcomeMessage) {
             const channel = member.guild.channels.cache.get(config.welcomeChannelId);
@@ -13,22 +17,34 @@ module.exports = {
                 const msg = config.welcomeMessage
                     .replace('{user}', `<@${member.id}>`)
                     .replace('{server}', member.guild.name);
-                channel.send({ content: msg });
+                
+                channel.send({ content: msg }).catch(error => {
+                    console.error(`Could not send welcome message to channel ${channel.id}:`, error);
+                });
             }
         }
+
         // Assign role
         if (config.welcomeRoleId) {
             const role = member.guild.roles.cache.get(config.welcomeRoleId);
             if (role) {
-                member.roles.add(role).catch(() => {});
+                // IMPROVED: Added error logging
+                member.roles.add(role).catch(error => {
+                    console.error(`Could not add role ${role.id} to user ${member.id}:`, error);
+                });
             }
         }
+
         // Optionally, send DM
         if (config.welcomeDM) {
             const msg = config.welcomeMessage
                 .replace('{user}', member.user.username)
                 .replace('{server}', member.guild.name);
-            member.send({ content: msg }).catch(() => {});
+            
+            // IMPROVED: Added error logging
+            member.send({ content: msg }).catch(error => {
+                console.error(`Could not send welcome DM to user ${member.id}:`, error);
+            });
         }
     },
 };
