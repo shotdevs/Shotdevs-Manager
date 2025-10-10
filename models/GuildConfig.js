7b0b25a9b34eac1df4ed35c999db2f17ea3894fe
Shibindevs <shibinhussainmk@gmail.com>
2025-10-10 07:40:01 +0530

    Update GuildConfig.js

--- models/GuildConfig.js
+++ models/GuildConfig.js
@@ -3,12 +3,17 @@ const { Schema, model } = require('mongoose');
 const guildConfigSchema = new Schema({
     guildId: { type: String, required: true, unique: true },
     staffRoleId: { type: String },
-    ticketPanelTitle: { type: String, default: 'Create a Ticket' },
-    ticketPanelDescription: { type: String, default: 'Please select a reason for opening a ticket.' },
+    ticketPanelTitle: { type: String },
+    ticketPanelDescription: { type: String },
     orderCategoryId: { type: String },
     enquiryCategoryId: { type: String },
     supportCategoryId: { type: String },
     ticketLogChannelId: { type: String },
+    // NEW WELCOME SYSTEM FIELDS
+    welcomeChannelId: { type: String },
+    welcomeRoleId: { type: String },
+    welcomeMessage: { type: String, default: 'Welcome {user} to {servername}!' },
+    welcomeEnabled: { type: Boolean, default: false },
 });
 
 module.exports = model('GuildConfig', guildConfigSchema);
