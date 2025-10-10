4546e79a053b2ca9fe5b66356ec92d11c2fad2cc
Shibindevs <shibinhussainmk@gmail.com>
2025-10-10 06:57:27 +0530

    Update set-log-channel.js

--- commands/admin/set-log-channel.js
+++ commands/admin/set-log-channel.js
@@ -4,13 +4,18 @@ const { setConfig } = require('../../configManager');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('set-log-channel')
-        .setDescription('Set the channel where ticket logs will be sent.')
+        .setDescription('Sets the channel where ticket logs will be sent.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addChannelOption(option => option.setName('channel').setDescription('The text channel for logs').addChannelTypes(ChannelType.GuildText).setRequired(true)),
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('The text channel for logs')
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)),
     async execute(interaction) {
-        const guildId = interaction.guild.id;
         const channel = interaction.options.getChannel('channel');
-        setConfig(guildId, 'ticketLogChannelId', channel.id);
-        await interaction.reply({ content: `✅ Ticket support role has been set to **${channel.name}**.`, flags: [ 'Ephemeral' ] });
-       },
+        // This now correctly saves the 'ticketLogChannelId'
+        await setConfig(interaction.guild.id, 'ticketLogChannelId', channel.id);
+        // This now sends the correct reply message
+        await interaction.reply({ content: `✅ Ticket log channel has been set to **${channel.name}**.`, flags: [ 'Ephemeral' ] });
+    },
 };
