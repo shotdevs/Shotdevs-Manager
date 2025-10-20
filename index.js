c704402a2e5b7830bace1b083ab505b9e6b85eb8
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2025-10-20 22:13:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function start() {
         const port = process.env.PORT || 3000;
 
         app.set('view engine', 'ejs');
-        app.set('views', path.join(__name, 'views'));
+        app.set('views', path.join(__dirname, 'views'));
         app.use(express.static(path.join(__dirname, 'public')));
 
         app.get('/', (req, res) => {
